c876ba6beb24ec1fd7e41012849f2c1c21f3ff09
shivani.mishra <shivani.mishra@dsv.com>
2022-07-10 20:57:47 +0200

    clean up

--- src/components/dropdown/noOfCardSelect.tsx
+++ src/components/dropdown/noOfCardSelect.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
 interface IProps {
-    handleNoOfCards: (e:any)=>void
+    handleNoOfCards: (e: any) => void
 }
 
 const NoOfCardSelect: React.FC<IProps> = ({ handleNoOfCards }) => {
     return (
         <div>
-            <div className="row">
-                <div className="col-md-6 offset-md-3">
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label>Number of Cards :</label>
-                            <select className="form-control" name="city" onChange={handleNoOfCards}>
-                                <option selected>Select Number</option>
-                                <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="50">50</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
+            <div className="col-auto">
+            <label className="visually-hidden" htmlFor="autoSizingSelect">Cards</label>
+                <select className="form-select" name="cards" id="autoSizingSelect" onChange={handleNoOfCards}>
+                    <option selected>No Of Cards</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
--- src/components/dropdown/sortByParamSelect.tsx
+++ src/components/dropdown/sortByParamSelect.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 
 interface IProps {
-    handleSorting :(e)=>void
+    handleSorting: (e:any) => void
 }
 
-const SortByParamSelect : React.FC<IProps> = ({ handleSorting }) => {
+const SortByParamSelect: React.FC<IProps> = ({ handleSorting }) => {
     return (
-        <div>
-            <div className="row">
-                <div className="col-md-6 offset-md-3">
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label>Sort By :</label>
-                            <select className="form-control" name="city" onChange={handleSorting}>
-                                <option selected>Select</option>
-                                <option value="name">Name</option>
-                                <option value="height">Height</option>
-                                <option value="weight">Weight</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div className="col-auto">
+            <label className="visually-hidden" htmlFor="autoSizingSelect">Cards</label>
+            <select className="form-select" name="cards" id="autoSizingSelect" onChange={handleSorting}>
+                <option selected>Sort By</option>
+                <option value="name">Name</option>
+                <option value="height">Height</option>
+                <option value="weight">Weight</option>
+            </select>
         </div>
+
     );
 };
 
