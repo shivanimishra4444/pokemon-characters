8ea6dea1357cd610c7ef67d5e0048782da6b12a8
shivani.mishra <shivani.mishra@dsv.com>
2022-07-10 20:56:38 +0200

    card details

--- src/components/card/cardDetails.tsx
+++ src/components/card/cardDetails.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import { getPokemonDetailsByName } from '@/config/api';
+import { capitalizeFirstLetter } from '@/helper/helper';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IDetail } from '../pokemon';
+import './cardDetails.scss'
+import { useHistory } from "react-router-dom";
 
-const cardDetails = () => {
+
+type NameParams = {
+    name: string;
+};
+const cardDetails: React.FC = () => {
+
+    const [moreDetails, setMoreDetails] = useState<IDetail>()
+    const { name } = useParams<NameParams>()
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const history = useHistory();
+
+    useEffect(() => {
+        const getDetails = async () => {
+            const result = await getPokemonDetailsByName(name)
+            setMoreDetails(result)
+            setLoading(false)
+        }
+        getDetails()
+    }, [])
     return (
-        <div>
-            
-        </div>
+        <>
+            {!loading ?
+                <section className='card-detail container'>
+                    <button type="button" className="btn btn-dark card-detail__button" onClick={history.goBack}>&larr; Back </button>
+                    <div className="card mb-3" >
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img className='card-detail__image' src={moreDetails.sprites.other["official-artwork"]["front_default"]} alt={moreDetails.name} />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h4 className="card-title">Hi, I am <b>{capitalizeFirstLetter(moreDetails.name)}</b>. Please find below more details!</h4>
+                                    <p className="card-text"><b>Height:</b>{` ${moreDetails.height} cm`}</p>
+                                    <p className="card-text"><b>weight: </b>{` ${moreDetails.weight} kg`}</p>
+                                    <p className="card-text"><b>Abilities: </b>
+                                        {moreDetails.abilities.map((ability, index) => {
+                                            return <span key={index}>{` ${ability.ability.name} ,`}</span>
+                                        })}
+                                    </p>
+                                    <p className="card-text"><b>Moves: </b>
+                                        {moreDetails.moves.map((move, index) => {
+                                            return <span key={index}>{` ${move.move.name} ,`}</span>
+                                        })}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                :
+                <div> Loading!!</div>
+            }
+        </>
+
     );
 };
 
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,27 +1,28 @@
+import { capitalizeFirstLetter } from '@/helper/helper';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './card.scss'
 interface IProps {
     pokemon: any
 }
 
-const Card: React.FC<IProps> = ({ pokemon }) => {
-
-    const capitalizeFirstLetter = (string: string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
+const Card: React.FC<IProps> = ({ pokemon }) => {   
     return (
         <div className="character col-sm-3 ">
             <div className="card">
                 <h5 className="card-header bg-transparent border-success">{capitalizeFirstLetter(pokemon.name)}</h5>
                 <img src={pokemon.sprites.other["official-artwork"]["front_default"]} alt={pokemon.name} />
                 <div className="card-body">
-                    <p className="card-text">{pokemon.height}</p>
-                    <p className="card-text">{pokemon.weight}</p>
-                    {pokemon.abilities.map(ability => {
-                        return <p className="card-text">{ability.ability.name}</p>
+                    <p className="card-text"><b>Height:</b>{` ${pokemon.height} cm`}</p>
+                    <p className="card-text"><b>Weight: </b>{` ${pokemon.weight} kg`}</p>
+                    <p className="card-text"><b>Abilities: </b>
+                    {pokemon.abilities.map((ability, index )=> {
+                        return <span key={index}>{` ${ability.ability.name} ,`}</span>
                     })}
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </p>
+                    <Link to={`/detail/${pokemon.name}`} className="btn btn-primary">
+                        Explore
+                    </Link>
                 </div>
             </div>
         </div>
